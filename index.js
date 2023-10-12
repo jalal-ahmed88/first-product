1629815a1a8a76d5f8d4b5fe49a1c2da3c3bb722
jalal-ahmed88 <jamnazeerahmed9@gmail.com>
2023-10-12 00:17:52 -0700

    variable

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Declare variables with explicit types
+let sname = "John";
+let age = 30;
+// You can also use the 'const' keyword for immutable values
+const pi = 3.14;
+// TypeScript can infer types based on the initial value
+let isActive = true; // TypeScript infers 'isActive' as a boolean
+console.log(sname, age, pi, isActive);
---
+++ index.ts
@@ -0,0 +1,15 @@
+// Declare variables with explicit types
+
+let sname: string = "John";
+
+let age: number = 30;
+
+// You can also use the 'const' keyword for immutable values
+
+const pi: number = 3.14;
+
+// TypeScript can infer types based on the initial value
+
+let isActive = true; // TypeScript infers 'isActive' as a boolean
+
+console.log(sname,age,pi,isActive);
